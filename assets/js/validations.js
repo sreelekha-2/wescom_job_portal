587404d63cefc2f724be4df234a5068dcb361284
Sreelekha <sreelekhakutikanti@gmail.com>
2024-02-05 09:53:41 +0530

    icomoon added for icons

--- assets/js/validations.js
+++ assets/js/validations.js
@@ -7,33 +7,13 @@ togglePassword.addEventListener('click', function (e) {
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
     // toggle the eye slash icon
-    togglePassword.classList.toggle('fa-eye');
+    togglePassword.classList.toggle('icon-eye-slash');
+    togglePassword.classList.toggle('icon-eye');
 });
 
 
-// const countryData=[
-//     {url:"../../../images/afghanisthan.png",countryCode:"+93"},
-//     {url:"../../../images/australia.png",countryCode:"+93"},
-//     {url:"../../../images/hangkong.png",countryCode:"+93"},
-//     {url:"../../../images/india.png",countryCode:"+93"},
-//     {url:"../../../images/jordan.png",countryCode:"+93"}
-// ];
-// let listData = ""
-// countryData.forEach(({ url }) => {
 
-//     listItem = "<li class='dropdown-item'><img src=" + url + "/></li>"
-//     listData = listData + listItem
-// }
-// )
 
-// // console.log
-// document.getElementById("countriesList").innerHTML=listData
-
-// window.intlTelInput(input, {
-//     initialCountry: "us",
-//     showSelectedDialCode: true,
-    
-//   });
 
 // $(document).ready(function() {
 //     $('.js-example-basic-single').select2();
